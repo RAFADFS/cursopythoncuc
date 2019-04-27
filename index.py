5f09d491350266286b5592bcfb4a6d36bf0eb114
Rafael <rafadfs91@gmail.com>
2019-04-27 10:42:10 -0500

    Tipos de datos, conversion

--- index.py
+++ index.py
@@ -4,5 +4,46 @@ Editor de Spyder
 
 Este es un archivo temporal.
 """
-a = 5
-A = 7
+# comentario de linea
+"""
+ comentario de parrafo
+"""
+
+# Hola mundo
+print("hola mundo")
+
+# Variables
+
+x = 4  # int
+y = "Texto"  # string
+z = 4.5  # float
+a = True  # boolean
+b = False  # boolean
+c = 4+5j  # complex
+
+print(x, y, z, a, b, c)
+# solo cuando son str
+# print(x+' '+y+' '+z+' '+a+' '+b+' '+c)
+
+# Conocer el tipo de datos de una variable
+print(type(b))
+
+# Conversiones de tipos de datos
+
+# Enteros
+x = int(2)
+print(x)
+x = int(2.8)
+print(x)
+x = int("2")
+print(x)
+
+# Float
+x = float(2)
+print(x)
+x = float(2.8)
+print(x)
+x = float("2")
+print(x)
+x = float("2.5")
+print(x)
